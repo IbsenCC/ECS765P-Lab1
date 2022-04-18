a717e7e85522480894924d17e669f82763b81e14
IbsenCC <endymion.zyf@gmail.com>
2022-04-18 02:27:22 +0100

    update

--- src/main/java/server/GrpcServer.java
+++ src/main/java/server/GrpcServer.java
@@ -9,9 +9,9 @@ public class GrpcServer {
         Server server = ServerBuilder
             .forPort(8080)
             .addService(new MatrixServiceImpl()).build();
-	    System.out.println("Starting server...");
+        System.out.println("Starting server...");
         server.start();
-	    System.out.println("Server started!");
+        System.out.println("Server started!");
         server.awaitTermination();
     }
 }
